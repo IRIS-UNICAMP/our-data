31bc9f0db2330a4b6072f5b27c9db30741eb7967
AlexandreOkita <xande.okita@gmail.com>
2021-05-09 19:08:24 -0300

    update hello_ourdata

--- ourdata/hello_ourdata.py
+++ ourdata/hello_ourdata.py
@@ -2,6 +2,7 @@ import requests
 
 def hello_data():
     print("Hello Data!")
+    print("Hello Data Again!")
 
 def test_request():
     print(requests.get("https://rickandmortyapi.com/api/character/?page=2"))
